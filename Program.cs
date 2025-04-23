852a20544b456052ec0cf75639d58db141a21af2
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-04-23 10:07:22 +0400

    code all menu features in the system

--- Program.cs
+++ Program.cs
@@ -4,7 +4,148 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool UsersSystemMenu = true;
+            // while loop to display the mnue ewhile the flag is true
+            while (UsersSystemMenu)
+            {
+                // display menu user of system list
+                Console.Clear();
+                Console.WriteLine("Select User Types:");
+                Console.WriteLine("1. User");
+                Console.WriteLine("2. Admin");
+                Console.WriteLine("0. Exist");
+                char choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                // user switch method to select one of many code blocks to be executed.
+                switch (choice)
+                {
+                    // case to display user menu
+                    case '1':
+                        UserMenu();
+                        break;
+                    // case to display Admin menu
+                    case '2':
+                        AdminMenu();
+                        break;
+                    // case to Exist from whole system
+                    case '0':
+                        UsersSystemMenu = false;
+                        break;
+                    // by default case to display error choic message 
+                    default:
+                        Console.WriteLine("The choice number is uncorrect, please try agine");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        // User Menu
+        public static void UserMenu()
+        {
+            bool inUserMenu = true;
+            // while loop to display the mnue ewhile the flag is true 
+            while (inUserMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("\n------ User Menu ------");
+                Console.WriteLine("1. Request Account Creation");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. View Balance");
+                Console.WriteLine("5. Submit Review/Complaint");
+                Console.WriteLine("0. Return to Main Menu");
+                Console.Write("Select option: ");
+                char userChoice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (userChoice)
+                {
+                    // case to Request Account Creation
+                    case '1':
+
+                        break;
+                    // case to Deposit
+                    case '2':
+
+                        break;
+                    // case to Withdraw
+                    case '3':
+
+                        break;
+                    // case to View Balance
+                    case '4':
+
+                        break;
+                    // case to Submit Review/Complaint
+                    case '5':
+
+                        break;
+                    // case to exist from user menu and Return to Main Menu 
+                    case '0':
+                        inUserMenu = false; // this will exit the loop and return
+                        break;
+                    // default case if user choice the wronge number within the range of cases 
+                    default:
+                        Console.WriteLine("Wronge Choice number, Try Agine!");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
+
+        }
+
+        // Admin Menu
+        public static void AdminMenu()
+        {
+            bool InAdminMenu = true;
+            // while loop to display the mnue ewhile the flag is true
+            while (InAdminMenu)
+            {
+                // display All Admin Menu
+                Console.WriteLine("\n------ Admin Menu ------");
+                Console.WriteLine("1. Process Next Account Request");
+                Console.WriteLine("2. View Submitted Reviews");
+                Console.WriteLine("3. View All Accounts");
+                Console.WriteLine("4. View Pending Account Requests");
+                Console.WriteLine("0. Return to Main Menu");
+                Console.Write("Select option: ");
+                char adminChoice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                // use switch to select one of many code blocks to be executed
+                switch (adminChoice)
+                {
+                    // case to Process Next Account Request
+                    case '1':
+
+                        break;
+                    // case to View Submitted Reviews
+                    case '2':
+
+                        break;
+                    // case to View All Accounts
+                    case '3':
+
+                        break;
+                    // case to View Pending Account Requests
+                    case '4':
+                        
+                        break;
+                    // case to Return to Main Menu
+                    case '0':
+                        InAdminMenu = false; // this will exit the loop and return
+                        break;
+                    // default case to display message to the admin if selected the wronge number
+                    default:
+                        Console.WriteLine("Wronge choice number, Try Agine!");
+                        Console.ReadKey();
+                        break;
+
+                }
+            }
+
         }
     }
 }
